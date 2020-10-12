d1bca6443ff26b67086c7643da43cdb8c5211f62
root <root@ca.server>
2020-10-13 00:52:51 +0300

    v1.0.3

--- bin/actions/download.js
+++ bin/actions/download.js
@@ -2,15 +2,22 @@
 	const fs = require('fs');	
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 	var caFile=(fs.existsSync(__root+'data/root_ca.crt'))?fs.readFileSync(__root+'data/root_ca.crt'):'';
+	var caFileUtf8=(fs.existsSync(__root+'data/root_ca.crt'))?fs.readFileSync(__root+'data/root_ca.crt','utf8'):'';
 	var caFileSize=(fs.existsSync(__root+'data/root_ca.crt'))?fs.statSync(__root+'data/root_ca.crt').size:0;
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 var lib={
 	init:function($){
-		$.response.setHeader("Content-disposition","attachment; filename=root_ca.crt");
-		$.response.setHeader('Content-Type', 'application/x-x509-ca-cert');
-		$.response.setHeader('Content-Length', caFileSize);
-		$.response.write(caFile);
-		$.response.end();
+		if($.account!==false){
+			let result={ca: caFileUtf8};
+			$.return({success:true,result:result});
+		}else{
+			$.response.setHeader("Content-disposition","attachment; filename=root_ca.crt");
+			$.response.setHeader('Content-Type', 'application/x-x509-ca-cert');
+			$.response.setHeader('Content-Length', caFileSize);
+			$.response.write(caFile);
+			$.response.end();
+		}
+		
 	}
 }
 module.exports=lib;

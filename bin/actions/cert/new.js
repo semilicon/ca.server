7afe3069391f40f475517ea371949d5c440460bf
root <root@ca.server>
2020-10-29 03:57:55 +0300

    v1.0.5

--- bin/actions/cert/new.js
+++ bin/actions/cert/new.js
@@ -5,6 +5,7 @@ var init=function($){
 	if($.DATA['organization'])options.organization=$.DATA['organization'];
 	if($.DATA['locality'])options.locality=$.DATA['locality'];
 	if($.DATA['countryCode'])options.countryCode=$.DATA['countryCode'];
+	if($.DATA['altnames'])options.altnames=$.DATA['altnames'];
 	let result=openssl.createNewCert(options);
 	$.return({success:true,result:result});
 }
--- bin/actions/cert/signcsr.js
+++ bin/actions/cert/signcsr.js
@@ -3,11 +3,14 @@ const openssl=require(__path+'lib/openssl.lib.js');
 var init=function($){
 	if(!$.DATA['csr'])return $.return({success:false,error:400});
 	let csr=$.DATA['csr'];
+	let altnames=$.DATA['altnames']||[];
 	let name=openssl.genTmpNeme();
 	fs.writeFileSync(__root+'data/.tmp/'+name+'.csr', csr);
+	if(altnames.length>0)openssl.altnamesCnf(name,altnames);
 	try{
-		openssl.signCsr(name);
+		openssl.signCsr(name,(altnames.length>0)?true:false);
 	}catch(err){
+		console.log(err);
 		return $.return({success:false,error:422});
 	}
 	let result={
